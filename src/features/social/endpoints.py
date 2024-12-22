a1ac9ead7764cd15777f2d3e575b588c37e8d701
Nino.Swag <nino.chang@swag.live>
2024-12-22 15:11:16 +0800

    Create `create_access_token`

--- src/features/social/endpoints.py
+++ src/features/social/endpoints.py
@@ -11,14 +11,17 @@ from . import application as app
 from src import config, dependencies
 from src.schemas import User, Token
 
-def create_access_token(data: dict, expires_delta: datetime.timedelta | None = None):
+def create_access_token(data: dict, exp: datetime.timedelta | int = None):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.datetime.now(datetime.UTC) + expires_delta
-    else:
-        expire = datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=15)
-    to_encode.update({"exp": expire})
+    
+    if isinstance(exp, int):
+        exp = datetime.datetime.fromtimestamp(exp, datetime.UTC)
+
+    exp = exp or datetime.datetime.now(datetime.UTC) + config.ACCESS_TOKEN_EXPIRE_TTL
+
+    to_encode.update({'exp': exp})
     encoded_jwt = jwt.encode(to_encode, config.SECRET_KEY, algorithm='HS256')
+    
     return encoded_jwt
 
 
@@ -27,9 +30,8 @@ async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ) -> Token:
     user = User(id=form_data.username, username=form_data.password)
-    access_token_expires = config.ACCESS_TOKEN_EXPIRE_TTL
     access_token = create_access_token(
-        data={'sub': user.username}, expires_delta=access_token_expires
+        data={'sub': user.username},
     )
     return Token(access_token=access_token, token_type='bearer')
 
